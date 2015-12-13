eeac9b84306a53e7cbe789774a5bcdb642a69d59
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-12-13 03:16:36 +0100

    Revert "settings: Add developer setting for root access"
    This reverts commit 3c6c825d33fe52ea7ccc480f0de70861544df19c.

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -165,9 +165,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String OPENGL_TRACES_KEY = "enable_opengl_traces";
 
-    private static final String ROOT_ACCESS_KEY = "root_access";
-    private static final String ROOT_ACCESS_PROPERTY = "persist.sys.root_access";
-
     private static final String IMMEDIATELY_DESTROY_ACTIVITIES_KEY
             = "immediately_destroy_activities";
     private static final String APP_PROCESS_LIMIT_KEY = "app_process_limit";
@@ -263,9 +260,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private SwitchPreference mShowAllANRs;
 
     private ColorModePreference mColorModePreference;
-    private PreferenceScreen mProcessStats;
-    private ListPreference mRootAccess;
-    private Object mSelectedRootValue;
 
     private DropDownPreference mNightModePreference;
 
@@ -286,7 +280,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private Dialog mAdbKeysDialog;
     private boolean mUnavailable;
-    private Dialog mRootDialog;
 
     @Override
     protected int getMetricsCategory() {
@@ -421,12 +414,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             removePreferenceForProduction(hdcpChecking);
         }
 
-        mRootAccess = (ListPreference) findPreference(ROOT_ACCESS_KEY);
-        mRootAccess.setOnPreferenceChangeListener(this);
-        if (!removeRootOptionsIfRequired()) {
-            mAllPrefs.add(mRootAccess);
-        }
-
         mColorModePreference = (ColorModePreference) findPreference(KEY_COLOR_MODE);
         mColorModePreference.updateCurrentAndSupported();
         if (mColorModePreference.getTransformsCount() < 2) {
@@ -495,18 +482,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mAllPrefs.add(pref);
         mResetSwitchPrefs.add(pref);
         return pref;
-    }
-
-    private boolean removeRootOptionsIfRequired() {
-        // user builds don't get root, and eng always gets root
-        if (!(Build.IS_DEBUGGABLE || "eng".equals(Build.TYPE))) {
-            if (mRootAccess != null) {
-                getPreferenceScreen().removePreference(mRootAccess);
-                return true;
-            }
-        }
-
-        return false;
     }
 
     @Override
@@ -688,7 +663,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateMobileDataAlwaysOnOptions();
         updateSimulateColorSpace();
         updateUSBAudioOptions();
-        updateUSBAudioOptions();
     }
 
     private void resetDangerousOptions() {
@@ -702,7 +676,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
         resetDebuggerOptions();
         writeLogdSizeOption(null);
-        resetRootAccessOptions();
         writeAnimationScaleOption(0, mWindowAnimationScale, null);
         writeAnimationScaleOption(1, mTransitionAnimationScale, null);
         writeAnimationScaleOption(2, mAnimatorDurationScale, null);
@@ -733,47 +706,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mKeepScreenOn.setValue(values[index]);
         mKeepScreenOn.setSummary(summaries[index]);
         mKeepScreenOn.setOnPreferenceChangeListener(this);
-    }
-
-   private void updateRootAccessOptions() {
-        String value = SystemProperties.get(ROOT_ACCESS_PROPERTY, "0");
-        mRootAccess.setValue(value);
-        mRootAccess.setSummary(getResources()
-                .getStringArray(R.array.root_access_entries)[Integer.valueOf(value)]);
-    }
-
-    public static boolean isRootForAppsEnabled() {
-        int value = SystemProperties.getInt(ROOT_ACCESS_PROPERTY, 0);
-        boolean daemonState =
-                SystemProperties.get("init.svc.su_daemon", "absent").equals("running");
-        return daemonState && (value == 1 || value == 3);
-    }
-
-    private void writeRootAccessOptions(Object newValue) {
-        String oldValue = SystemProperties.get(ROOT_ACCESS_PROPERTY, "0");
-        SystemProperties.set(ROOT_ACCESS_PROPERTY, newValue.toString());
-        if (Integer.valueOf(newValue.toString()) < 2 && !oldValue.equals(newValue)
-                && "1".equals(SystemProperties.get("service.adb.root", "0"))) {
-            SystemProperties.set("service.adb.root", "0");
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_ENABLED, 0);
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_ENABLED, 1);
-        }
-        updateRootAccessOptions();
-    }
-
-    private void resetRootAccessOptions() {
-        String oldValue = SystemProperties.get(ROOT_ACCESS_PROPERTY, "0");
-        SystemProperties.set(ROOT_ACCESS_PROPERTY, "0");
-        if (!oldValue.equals("0") && "1".equals(SystemProperties.get("service.adb.root", "0"))) {
-            SystemProperties.set("service.adb.root", "0");
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_ENABLED, 0);
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_ENABLED, 1);
-        }
-        updateRootAccessOptions();
     }
 
     private void updateHdcpValues() {
@@ -1883,23 +1815,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             return true;
         } else if (preference == mKeepScreenOn) {
             writeStayAwakeOptions(newValue);
-        } else if (preference == mRootAccess) {
-            if ("0".equals(SystemProperties.get(ROOT_ACCESS_PROPERTY, "0"))
-                    && !"0".equals(newValue)) {
-                mSelectedRootValue = newValue;
-                mDialogClicked = false;
-                if (mRootDialog != null) {
-                    dismissDialogs();
-                }
-                mRootDialog = new AlertDialog.Builder(getActivity())
-                        .setMessage(getResources().getString(R.string.root_access_warning_message))
-                        .setTitle(R.string.root_access_warning_title)
-                        .setPositiveButton(android.R.string.yes, this)
-                        .setNegativeButton(android.R.string.no, this).show();
-                mRootDialog.setOnDismissListener(this);
-            } else {
-                writeRootAccessOptions(newValue);
-            }
             return true;
         } else if (preference == mChamberUnlocked) {
             Settings.Secure.putInt(getActivity().getContentResolver(),
@@ -1926,10 +1841,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         if (mEnableDialog != null) {
             mEnableDialog.dismiss();
             mEnableDialog = null;
-        }
-        if (mRootDialog != null) {
-            mRootDialog.dismiss();
-            mRootDialog = null;
         }
     }
 
@@ -1974,13 +1885,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 // Reset the toggle
                 mSwitchBar.setChecked(false);
             }
-        } else if (dialog == mRootDialog) {
-            if (which == DialogInterface.BUTTON_POSITIVE) {
-                writeRootAccessOptions(mSelectedRootValue);
-            } else {
-                // Reset the option
-                writeRootAccessOptions("0");
-            }
         }
     }
 
@@ -1999,9 +1903,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 mSwitchBar.setChecked(false);
             }
             mEnableDialog = null;
-        } else if (dialog == mRootDialog) {
-            updateRootAccessOptions();
-            mRootDialog = null;
         }
     }
 
