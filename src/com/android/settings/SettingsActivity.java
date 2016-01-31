mintkat/packages_apps_Settings

Settings: Privacy Guard [2/2]

src/com/android/settings/SettingsActivity.java
@@ -118,6 +118,7 @@ import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
+import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
@@ -284,7 +285,8 @@ public class SettingsActivity extends Activity
             R.id.home_settings,
             R.id.dashboard,
             R.id.bitsyko_layers,
-            R.id.main_settings
+            R.id.main_settings,
+            R.id.privacy_settings_cyanogenmod
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -362,9 +364,9 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             MainSettings.class.getName(),
             DisplayRotation.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
     };
 
 

SuperKangerRom/android_packages_apps_Settings-AOSP

src/com/android/settings/SettingsActivity.java
@@ -120,6 +120,7 @@ import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
+import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
@@ -294,7 +295,8 @@ public class SettingsActivity extends Activity
             R.id.about_settings,
             R.id.accessibility_settings,
             R.id.print_settings,
-            R.id.dashboard
+            R.id.dashboard,
+            R.id.privacy_settings_vrtoxin
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -388,7 +390,8 @@ public class SettingsActivity extends Activity
             NotificationManagerSettings.class.getName(),
             ScreenPinningSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            PowerMenuSettings.class.getName()
+            PowerMenuSettings.class.getName(),
+            com.android.settings.vrtoxin.PrivacySettings.class.getName()
     };
 
 