780c04e294d747770a4ace43eb3772741803e555
rogersb11 <brettrogers11@gmail.com>
2016-02-10 13:41:41 +0100

    Settings: Owner info size 1/2
    Also move lockclock features out of weather on/off declaration

--- src/com/android/settings/vrtoxin/LockS.java
+++ src/com/android/settings/vrtoxin/LockS.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 CyanideL
+/* 
+ * Copyright (C) 2015 The Vrtoxin Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,37 +16,90 @@
 
 package com.android.settings.vrtoxin;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.SwitchPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.android.settings.OwnerInfoSettings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.settings.vrtoxin.SeekBarPreference;
 import com.android.internal.logging.MetricsLogger;
+import com.android.internal.widget.LockPatternUtils;
 
-public class LockS extends SettingsPreferenceFragment {
+public class LockS extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
+    private static final String OWNER_INFO_FONT_SIZE  = "owner_info_font_size";
+
+    private static final int MY_USER_ID = UserHandle.myUserId();
+    private LockPatternUtils mLockPatternUtils;
+    private Preference mOwnerInfoPref;
+    private SeekBarPreference mOwnerSize;
+
+    private boolean mIsPrimary;
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.vrtoxin_ls_shortcuts);
+        mResolver = getActivity().getContentResolver();
+
+        // Add options for device encryption
+        mIsPrimary = MY_USER_ID == UserHandle.USER_OWNER;
+
+        mOwnerInfoPref = findPreference(KEY_OWNER_INFO_SETTINGS);
+        if (mOwnerInfoPref != null) {
+            mOwnerInfoPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    OwnerInfoSettings.show(LockS.this);
+                    return true;
+                }
+            });
+        }
+
+        mOwnerSize = (SeekBarPreference) findPreference(OWNER_INFO_FONT_SIZE);
+        mOwnerSize.setValue(Settings.System.getInt(mResolver,
+                Settings.System.OWNER_INFO_FONT_SIZE, 14));
+        mOwnerSize.setOnPreferenceChangeListener(this);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mOwnerSize) {
+            int width = ((Integer)newValue).intValue();
+            Settings.System.putInt(mResolver,
+                    Settings.System.OWNER_INFO_FONT_SIZE, width);
+            return true;
+        }
         return false;
-    }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;
- 		return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     @Override
